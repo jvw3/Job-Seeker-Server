fed83c517e6ee974014d88a2a3feb6a15963ce2b
Jonathan Woodard <jvw.226@gmail.com>
2022-12-06 10:57:42 -0600

    updated data-relationships, models, and fixtures

--- jobseekerapi/models/interview.py
+++ jobseekerapi/models/interview.py
@@ -3,7 +3,8 @@ from django.db import models
 class Interview(models.Model):
     """This class creates an instance of an interview"""
 
-    board_job = models.ForeignKey("BoardJob", on_delete=models.CASCADE)
+    board_job = models.ForeignKey("BoardJob", on_delete=models.CASCADE, related_name="interviews")
+    prep = models.ForeignKey("InterviewPrep", on_delete=models.CASCADE, related_name="prep")
     date = models.DateTimeField()
     is_complete = models.BooleanField()
     interview_feedback = models.TextField(max_length=10000)
