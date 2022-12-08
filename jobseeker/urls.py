5181d752cc5711bbce4c2747b7efb9a6232caac4
Jonathan Woodard <jvw.226@gmail.com>
2022-12-08 11:05:29 -0600

    created foundation for jobseeker urls

--- jobseeker/urls.py
+++ jobseeker/urls.py
@@ -17,16 +17,24 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from jobseekerapi.views import BoardView, CategoryView
+from jobseekerapi.views import BoardView, CategoryView, CompanyView, QuestionView, register_user, login_user, JobView, InterviewPrepView, CustomPrepView, BoardJobView, InterviewView
 
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'boards', BoardView, 'board')
 router.register(r'categories', CategoryView, 'category')
+router.register(r'companies', CompanyView, 'company')
+router.register(r'questions', QuestionView, 'question')
+router.register(r'jobs', JobView, 'job')
+router.register(r'preps', InterviewPrepView, 'prep')
+router.register(r'customs', CustomPrepView, 'custom')
+router.register(r'boardjobs', BoardJobView, 'boardjob')
+router.register(r'interviews', InterviewView, 'interview')
+
 
 urlpatterns = [
-    # path('register', register_user),
-    # path('login', login_user),
+    path('register', register_user),
+    path('login', login_user),
     path('admin/', admin.site.urls),
     path('', include(router.urls))
 ]
