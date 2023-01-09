0861155550af8ad54c060a569f214b9a81d6089b
Jonathan Woodard <jvw.226@gmail.com>
2023-01-08 20:50:12 -0600

    created network_meeting model

--- jobseekerapi/models/network_meeting.py
+++ jobseekerapi/models/network_meeting.py
@@ -3,9 +3,9 @@ from django.db import models
 class NetworkMeeting(models.Model):
     """This class creates an instance of a company. A company resource is only used in association with a BoardJob """
 
-    contact = models.ForeignKey("Contact", on_delete=models.CASCADE, related_name="calls")
+    contact = models.ForeignKey("Contact", on_delete=models.CASCADE, related_name="meetings")
     description = models.CharField(max_length=500)
-    meeting_date = models.DateField()
+    meeting_date = models.DateTimeField()
     notes = models.TextField(max_length=10000, blank=True)
     is_complete = models.BooleanField()
     meeting_type = models.ForeignKey("MeetingType", on_delete=models.CASCADE)
