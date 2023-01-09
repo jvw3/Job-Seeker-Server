804e3a4af548b45d3ea7df46ace235c1399056d1
Jonathan Woodard <jvw.226@gmail.com>
2023-01-08 20:45:56 -0600

    added work_status property to boardjob model, and added job_score custom property to board_job model

--- jobseekerapi/models/board_job.py
+++ jobseekerapi/models/board_job.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from .board import Board
+from .priority_rank import PriorityRank
 
 
 class BoardJob(models.Model):
@@ -16,21 +18,139 @@ class BoardJob(models.Model):
     received_offer = models.BooleanField()
     salary = models.IntegerField()
     location = models.CharField(max_length=70)
+    work_status = models.CharField(max_length=50)
     salary_rating = models.PositiveIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(10)])
     location_rating = models.PositiveIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(10)])
     culture_rating = models.PositiveIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(10)])
     leadership_rating = models.PositiveIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(10)])
     team_rating = models.PositiveIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(10)])
-    job_score = models.IntegerField(null=True, blank=True)
     board = models.ForeignKey("Board", on_delete=models.CASCADE, related_name="jobs")
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
     tags = models.ManyToManyField("Tag", through="BoardJobTag", related_name='tags')
 
 
     @property
-    def joined(self):
-        return self.__joined
+    def job_score(self):
+        board = self.board
+        ranks = PriorityRank.objects.filter(board=board.id)
 
-    @joined.setter
-    def joined(self, value):
-        self.__joined = value
+        final_score = 0
+        for priority_rank in ranks:
+            if priority_rank.rank_value == 1:
+                if priority_rank.name in "salary_rating":
+                    adjusted_salary_score = self.salary_rating * 0.4
+                    final_score += adjusted_salary_score
+                
+                elif priority_rank.name in "location_rating":
+                    adjusted_location_score = self.location_rating * 0.4
+                    final_score += adjusted_location_score
+                
+                elif priority_rank.name in "culture_rating":
+                    adjusted_culture_score = self.culture_rating * 0.4
+                    final_score += adjusted_culture_score
+                
+                elif priority_rank.name in "leadership_rating":
+                    adjusted_leadership_score = self.leadership_rating * 0.4
+                    final_score += adjusted_leadership_score
+                
+                elif priority_rank.name in "team_rating":
+                    adjusted_team_score = self.team_rating * 0.4
+                    final_score += adjusted_team_score
+
+
+
+            elif priority_rank.rank_value == 2:
+                if priority_rank.name in "salary_rating":
+                    adjusted_salary_score = self.salary_rating * 0.3
+                    final_score += adjusted_salary_score
+                
+                elif priority_rank.name in "location_rating":
+                    adjusted_location_score = self.location_rating * 0.3
+                    final_score += adjusted_location_score
+                
+                elif priority_rank.name in "culture_rating":
+                    adjusted_culture_score = self.culture_rating * 0.3
+                    final_score += adjusted_culture_score
+                
+                elif priority_rank.name in "leadership_rating":
+                    adjusted_leadership_score = self.leadership_rating * 0.3
+                    final_score += adjusted_leadership_score
+                
+                elif priority_rank.name in "team_rating":
+                    adjusted_team_score = self.team_rating * 0.3
+                    final_score += adjusted_team_score
+
+            
+            
+            elif priority_rank.rank_value == 3:
+                if priority_rank.name in "salary_rating":
+                    adjusted_salary_score = self.salary_rating * 0.15
+                    final_score += adjusted_salary_score
+                
+                elif priority_rank.name in "location_rating":
+                    adjusted_location_score = self.location_rating * 0.15
+                    final_score += adjusted_location_score
+                
+                elif priority_rank.name in "culture_rating":
+                    adjusted_culture_score = self.culture_rating * 0.15
+                    final_score += adjusted_culture_score
+                
+                elif priority_rank.name in "leadership_rating":
+                    adjusted_leadership_score = self.leadership_rating * 0.15
+                    final_score += adjusted_leadership_score
+                
+                elif priority_rank.name in "team_rating":
+                    adjusted_team_score = self.team_rating * 0.15
+                    final_score += adjusted_team_score
+
+
+
+
+            elif priority_rank.rank_value == 4:
+                if priority_rank.name in "salary_rating":
+                    adjusted_salary_score = self.salary_rating * 0.10
+                    final_score += adjusted_salary_score
+                
+                elif priority_rank.name in "location_rating":
+                    adjusted_location_score = self.location_rating * 0.10
+                    final_score += adjusted_location_score
+                
+                elif priority_rank.name in "culture_rating":
+                    adjusted_culture_score = self.culture_rating * 0.10
+                    final_score += adjusted_culture_score
+                
+                elif priority_rank.name in "leadership_rating":
+                    adjusted_leadership_score = self.leadership_rating * 0.10
+                    final_score += adjusted_leadership_score
+                
+                elif priority_rank.name in "team_rating":
+                    adjusted_team_score = self.team_rating * 0.10
+                    final_score += adjusted_team_score
+
+
+
+
+
+            elif priority_rank.rank_value == 5:
+                if priority_rank.name in "salary_rating":
+                    adjusted_salary_score = self.salary_rating * 0.05
+                    final_score += adjusted_salary_score
+                
+                elif priority_rank.name in "location_rating":
+                    adjusted_location_score = self.location_rating * 0.05
+                    final_score += adjusted_location_score
+                
+                elif priority_rank.name in "culture_rating":
+                    adjusted_culture_score = self.culture_rating * 0.05
+                    final_score += adjusted_culture_score
+                
+                elif priority_rank.name in "leadership_rating":
+                    adjusted_leadership_score = self.leadership_rating * 0.05
+                    final_score += adjusted_leadership_score
+                
+                elif priority_rank.name in "team_rating":
+                    adjusted_team_score = self.team_rating * 0.05
+                    final_score += adjusted_team_score
+                
+
+        return format(final_score, '.1f')
