fbffb5fe367d716adc92248d66acbbb0746b2dfd
Jonathan Woodard <jvw.226@gmail.com>
2023-01-08 20:50:29 -0600

    created priority_rank model

--- jobseekerapi/models/priority_rank.py
+++ jobseekerapi/models/priority_rank.py
@@ -4,9 +4,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class PriorityRank(models.Model):
     """This class creates an instance of a priority rank for a board"""
 
-    board = models.ForeignKey("Board", on_delete=models.CASCADE)
-    salary = models.PositiveIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(5)])
-    location = models.PositiveIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(5)])
-    culture = models.PositiveIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(5)])
-    leadership = models.PositiveIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(5)])
-    team = models.PositiveIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    board = models.ForeignKey("Board", on_delete=models.CASCADE, related_name="priorities")
+    name = models.CharField(max_length=20)
+    rank_value = models.PositiveIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(5)])
