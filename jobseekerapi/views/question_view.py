b085ff7a132105f89495f999dbb8398118c5ae94
Jonathan Woodard <jvw.226@gmail.com>
2023-01-08 21:32:35 -0600

    small syntax changes and adding comments

--- jobseekerapi/views/question_view.py
+++ jobseekerapi/views/question_view.py
@@ -1,4 +1,4 @@
-"""View module for handling requests for boards"""
+"""View module for handling requests for questions"""
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
