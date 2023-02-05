fafa2b7834a42abeed1cb7ac7149261056805cea
Jonathan Woodard <jvw.226@gmail.com>
2023-02-05 08:31:50 -0800

    updated network meeting to support uuid

--- jobseekerapi/models/network_meeting.py
+++ jobseekerapi/models/network_meeting.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.utils import timezone
+import uuid
 
 class NetworkMeeting(models.Model):
-    """This class creates an instance of a company. A company resource is only used in association with a BoardJob """
+    """This class creates an instance of a network meeting. A network meeting resource is only used in association with a contact"""
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     seeker = models.ForeignKey("Seeker", on_delete=models.CASCADE, related_name="meetings")
     contact = models.ForeignKey("Contact", on_delete=models.CASCADE, related_name="meetings")
     description = models.CharField(max_length=500)
